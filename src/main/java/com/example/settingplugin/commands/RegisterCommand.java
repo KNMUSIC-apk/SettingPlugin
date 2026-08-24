7d7c4eb5d5bd6c9e4b5b0ab01777010c242535f9
KNMUSIC-apk <music.vn.mc@gmail.com>
2026-08-24 12:41:33 +0700

    Remove blindness effect after successful registration
    Remove blindness effect upon successful registration.

--- src/main/java/com/example/settingplugin/commands/RegisterCommand.java
+++ src/main/java/com/example/settingplugin/commands/RegisterCommand.java
@@ -2,6 +2,7 @@ package com.example.settingplugin.commands;
 
 import com.example.settingplugin.SettingPlugin;
 import com.example.settingplugin.PlayerSettings;
+import com.example.settingplugin.listeners.LoginListener;
 import com.example.settingplugin.utils.PasswordHasher;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -50,6 +51,10 @@ public class RegisterCommand implements CommandExecutor {
         settings.setPasswordHash(hash);
         settings.setLoggedIn(true);
         plugin.getSettingsManager().saveSettings(player.getUniqueId());
+
+        // Xóa hiệu ứng mù khi đăng ký thành công
+        LoginListener.removeBlindness(player);
+
         player.sendMessage(ChatColor.GREEN + "Đăng ký thành công! Bạn đã đăng nhập.");
         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
         return true;
